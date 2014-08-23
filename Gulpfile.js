aa83765fcd60c48daf0b9f8702b8296657345764
Joe Curlee <joe.curlee@gmail.com>
2014-08-23 12:01:44 -0500

    converted coffee.md to litcoffee

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,4 @@
 // This file is exists as a work-around to allow us to write our gulpfile
 // using the almighty coffeescript.
 require('coffee-script/register');
-require('./gulpfile.coffee.md');
+require('./gulpfile.litcoffee');
